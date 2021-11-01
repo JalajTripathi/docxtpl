c8c811b13c613e055074ae463c57af7293908645
Jalaj Tripathi <37848682+JalajTripathi@users.noreply.github.com>
2021-11-01 15:07:39 +0530

    Add files via upload

--- generate.py
+++ generate.py
@@ -3,13 +3,8 @@ from docx.shared import Cm
 from docxtpl import DocxTemplate, InlineImage
 import json
 
-def get_context(formData):
-    """ You can generate your context separately since you may deal with a lot 
-        of documents. You can carry out computations, etc in here and make the
-        context look like the sample below.
-    """
+def get_agriculture_context(formData):
     gata = len(formData['चौहद्दी_गाटा_संख्या'])
-    # abc = dict()
     gata_data = []
     for i in range(gata):
         abc = [{
@@ -19,15 +14,8 @@ def get_context(formData):
                 "उत्तर": formData['उत्तर'][i],
                 "दक्षिण": formData['दक्षिण'][i] 
             }]
-        # abc.append(abc)
         gata_data += abc
-        # n = { "test"+str(i) : abc }
 
-    print("N: ", gata_data)
-            # print(abc)
-    # abc = json.loads(abc)
-    # print(abc)
-            
     return {
         'मालियत': formData['मालियत'],
         'विक्रय_मूल्य': formData['विक्रय_मूल्य'],
@@ -42,13 +30,10 @@ def get_context(formData):
         'विक्रेता_का_आधार' : formData['विक्रेता_का_आधार'],
         'विक्रेता_का_फ़ोन' : formData['विक्रेता_का_फ़ोन'],
         'विक्रेता_का_पैन' : formData['विक्रेता_का_पैन'],
-
-
         'क्रेता_का_विवरण': formData['क्रेता_का_विवरण'],
         'क्रेता_का_आधार' : formData['क्रेता_का_आधार'],
         'क्रेता_का_फ़ोन' : formData['क्रेता_का_फ़ोन'],
         'क्रेता_का_पैन' : formData['क्रेता_का_पैन'],
-
         'abc': gata_data,
         'a' : formData['अनुमति'],
         'ग्राम_कोड' : formData['ग्राम_कोड'],
@@ -68,23 +53,77 @@ def get_context(formData):
         'दिनांक' : formData['दिनांक']
     }
 
+def get_residential_context(formData):
+    gata = len(formData['चौहद्दी_गाटा_संख्या'])
+    gata_data = []
+    for i in range(gata):
+        abc = [{
+                "गाटा_संख्या" : formData['चौहद्दी_गाटा_संख्या'][i],
+                "पूरब" : formData['पूरब'][i],
+                "पश्चिम": formData['पश्चिम'][i],
+                "उत्तर": formData['उत्तर'][i],
+                "दक्षिण": formData['दक्षिण'][i] 
+            }]
+        gata_data += abc
+    cheque = len(formData['चेक_संख्या'])
+    cheque_data = []
+    for i in range(cheque):
+        cheque_json = [{
+                "चेक_संख्या" : formData['चेक_संख्या'][i],
+                "बैंक" : formData['बैंक'][i],
+                "शाखा": formData['शाखा'][i],
+                "मूल्य": formData['मूल्य'][i],
+                "दिनांकित": formData['दिनांकित'][i] 
+            }]
+        cheque_data += cheque_json
 
-def from_template(template, formData):
-    #target_file = StringIO()
+    return {
+        'मालियत': formData['मालियत'],
+        'विक्रय_मूल्य': formData['विक्रय_मूल्य'],
+        'विक्रय_मूल्य_शब्दों_में' : formData['विक्रय_मूल्य_शब्दों_में'],
+        'देय_स्टाम्प': formData['देय_स्टाम्प'],
+        'क्षेत्र_दर': formData['क्षेत्र_दर'],
+        'ग्राम': formData['ग्राम'],
+        'सम्पत्ति_का_विवरण': formData['सम्पत्ति_का_विवरण'],
+        'संपत्ति_का_क्षेत्रफल': formData['संपत्ति_का_क्षेत्रफल'],
+        'प्रतिफल_की_धनराशि': formData['प्रतिफल_की_धनराशि'],
+        'विक्रेता_का_विवरण': formData['विक्रेता_का_विवरण'],
+        'विक्रेता_का_आधार' : formData['विक्रेता_का_आधार'],
+        'विक्रेता_का_फ़ोन' : formData['विक्रेता_का_फ़ोन'],
+        'विक्रेता_का_पैन' : formData['विक्रेता_का_पैन'],
+        'क्रेता_का_विवरण': formData['क्रेता_का_विवरण'],
+        'क्रेता_का_आधार' : formData['क्रेता_का_आधार'],
+        'क्रेता_का_फ़ोन' : formData['क्रेता_का_फ़ोन'],
+        'क्रेता_का_पैन' : formData['क्रेता_का_पैन'],
+        'abc': gata_data,
+        'cheque_info' : cheque_data,
+        'अनुमति' : formData['अनुमति'],
+        'ग्राम_कोड' : formData['ग्राम_कोड'],
+        'अर्ध_विक्रय_मूल्य' : formData['अर्ध_विक्रय_मूल्य'],
+        'गवाहान_का_नाम_1' : formData['गवाहान_का_नाम_1'],
+        'गवाहान_का_पता_1' : formData['गवाहान_का_पता_1'],
+        'गवाहान_का_फ़ोन_1' : formData['गवाहान_का_फ़ोन_1'],
+        'गवाहान_का_पिता_का_नाम_1' : formData['गवाहान_का_पिता_का_नाम_1'],
+        'लिंग_1' : formData['लिंग_1'],
+        'गवाहान_का_नाम_2' : formData['गवाहान_का_नाम_2'],
+        'गवाहान_का_पता_2' : formData['गवाहान_का_पता_2'],
+        'गवाहान_का_फ़ोन_2' : formData['गवाहान_का_फ़ोन_2'],
+        'गवाहान_का_पिता_का_नाम_2' : formData['गवाहान_का_पिता_का_नाम_2'],
+        'लिंग_2' : formData['लिंग_2'],
+        'सड़क_की_स्थित' : formData['सड़क_की_स्थित'],
+        'मसविदाकर्ता' : formData['मसविदाकर्ता'],
+        'दिनांक' : formData['दिनांक']
+    }
 
+
+def from_template(template, formData, form_type):
+    print("Form Type: ", form_type)
     template = DocxTemplate(template)
-    context = get_context(formData)  # gets the context used to render the document
-
-    # print("Context: ", context)
-
-    # img_size = Cm(7)  # sets the size of the image
-    # sign = InlineImage(template, signature, img_size)
-    # context['signature'] = sign  # adds the InlineImage object to the context
-
+    if form_type == "agriculture":
+        context = get_agriculture_context(formData)
+    elif form_type == "residential":
+        context = get_residential_context(formData)
     target_file = BytesIO()
-    print("Debug 1", target_file)
     template.render(context)
     template.save(target_file)
-
-
     return target_file
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, send_file, render_template, request, make_response, redirect
 from flask.wrappers import Request
-import generate
+import generate, datetime
 
 import os
 import sys
@@ -16,7 +16,8 @@ else:
 @app.route("/")
 def index():
     if getCookie():
-        return render_template('dashboard.html')
+        print("Cookie Found")
+        return render_template('agreement_selection.html')
     else:
         return render_template("login.html")
 
@@ -32,9 +33,15 @@ def login():
         return render_template('login.html', error="Wrong combination of username and password! Try again!")
 
 def setCookie(username):
-    resp = make_response(redirect('/gen'))
+    resp = make_response(redirect('/agreement_selection'))
     resp.set_cookie('userID', username, secure = True, max_age=None)
     return resp
+
+@app.route("/agreement_selection", methods=['GET'])
+def agreement_selection():
+    if request.method == "GET":
+        return render_template('agreement_selection.html')
+    
 
 @app.route("/gen", methods=['POST', 'GET'])
 def gen_docx():
@@ -74,26 +81,82 @@ def gen_docx():
         formData['गवाहान_का_फ़ोन_2'] = request.form['गवाहान_का_फ़ोन_2']
         formData['गवाहान_का_पिता_का_नाम_2'] = request.form['गवाहान_का_पिता_का_नाम_2']
         formData['लिंग_2'] = request.form['लिंग_2']
-        # formData['लिंग_2'] = "पुत्र"
         formData['सड़क_की_स्थित'] = request.form['सड़क_की_स्थित']
         formData['मसविदाकर्ता'] = request.form['मसविदाकर्ता']
-        formData['दिनांक'] = request.form['दिनांक']
-
+        today_date = datetime.datetime.strptime(str(datetime.date.today()), "%Y-%m-%d").strftime("%d.%m.%Y")
+        formData['दिनांक'] = today_date
         half = formData['विक्रय_मूल्य']
         half = int(half)/2
-        
         formData['अर्ध_विक्रय_मूल्य'] = half
-
-        # print("FormData_1: ", formData['चौहद्दी_गाटा_संख्या'][1])
-        template = 'temp.docx'
-        document = generate.from_template(template, formData)
+        template = 'agriculture_template.docx'
+        document = generate.from_template(template, formData, form_type = "agriculture")
         document.seek(0)
         return send_file(
             document, mimetype='application/vnd.openxmlformats-'
             'officedocument.wordprocessingml.document', as_attachment=True,
             attachment_filename='Agriculture.docx')
     else:
-        return render_template('dashboard.html')
+        return render_template('agriculture.html')
+
+@app.route("/residential_form", methods=['POST', 'GET'])
+def residential():
+    if request.method=="POST":
+        formData = {}
+        formData['विक्रय_मूल्य'] = request.form['विक्रय_मूल्य']
+        formData['विक्रय_मूल्य_शब्दों_में'] = request.form['विक्रय_मूल्य_शब्दों_में']
+        formData['मालियत'] = request.form['मालियत']
+        formData['देय_स्टाम्प'] = request.form['देय_स्टाम्प']
+        formData['क्षेत्र_दर'] = request.form['क्षेत्र_दर']
+        formData['ग्राम_कोड'] = request.form['ग्राम_कोड']
+        formData['ग्राम'] = request.form['ग्राम']
+        formData['सम्पत्ति_का_विवरण'] = request.form['सम्पत्ति_का_विवरण']
+        formData['संपत्ति_का_क्षेत्रफल'] = request.form['संपत्ति_का_क्षेत्रफल']
+        formData['प्रतिफल_की_धनराशि'] = request.form['प्रतिफल_की_धनराशि']
+        formData['विक्रेता_का_विवरण'] = request.form['विक्रेता_का_विवरण']
+        formData['क्रेता_का_विवरण'] = request.form['क्रेता_का_विवरण']
+        formData['चौहद्दी_गाटा_संख्या'] = request.form.getlist('चौहद्दी_गाटा_संख्या[]')
+        formData['पूरब'] = request.form.getlist('पूरब[]')
+        formData['पश्चिम'] = request.form.getlist('पश्चिम[]')
+        formData['उत्तर'] = request.form.getlist('उत्तर[]')
+        formData['दक्षिण'] = request.form.getlist('दक्षिण[]')
+        formData['अनुमति'] = request.form['a']
+        formData['विक्रेता_का_आधार'] = request.form['विक्रेता_का_आधार']
+        formData['विक्रेता_का_फ़ोन'] = request.form['विक्रेता_का_फ़ोन']
+        formData['विक्रेता_का_पैन'] = request.form['विक्रेता_का_पैन']
+        formData['क्रेता_का_आधार'] = request.form['क्रेता_का_आधार']
+        formData['क्रेता_का_फ़ोन'] = request.form['क्रेता_का_फ़ोन']
+        formData['क्रेता_का_पैन'] = request.form['क्रेता_का_पैन']
+        formData['चेक_संख्या'] = request.form.getlist('चेक_संख्या[]')
+        formData['बैंक'] = request.form.getlist('बैंक[]')
+        formData['शाखा'] = request.form.getlist('शाखा[]')
+        formData['मूल्य'] = request.form.getlist('मूल्य[]')
+        formData['दिनांकित'] = request.form.getlist('दिनांकित[]')
+        formData['गवाहान_का_नाम_1'] = request.form['गवाहान_का_नाम_1']
+        formData['गवाहान_का_पता_1'] = request.form['गवाहान_का_पता_1']
+        formData['गवाहान_का_फ़ोन_1'] = request.form['गवाहान_का_फ़ोन_1']
+        formData['गवाहान_का_पिता_का_नाम_1'] = request.form['गवाहान_का_पिता_का_नाम_1']
+        formData['लिंग_1'] = request.form['लिंग_1']
+        formData['गवाहान_का_नाम_2'] = request.form['गवाहान_का_नाम_2']
+        formData['गवाहान_का_पता_2'] = request.form['गवाहान_का_पता_2']
+        formData['गवाहान_का_फ़ोन_2'] = request.form['गवाहान_का_फ़ोन_2']
+        formData['गवाहान_का_पिता_का_नाम_2'] = request.form['गवाहान_का_पिता_का_नाम_2']
+        formData['लिंग_2'] = request.form['लिंग_2']
+        formData['सड़क_की_स्थित'] = request.form['सड़क_की_स्थित']
+        formData['मसविदाकर्ता'] = request.form['मसविदाकर्ता']
+        today_date = datetime.datetime.strptime(str(datetime.date.today()), "%Y-%m-%d").strftime("%d-%m-%Y")
+        formData['दिनांक'] = today_date
+        half = formData['विक्रय_मूल्य']
+        half = int(half)/2
+        formData['अर्ध_विक्रय_मूल्य'] = half
+        template = 'residential_template.docx'
+        document = generate.from_template(template, formData, form_type = "residential")
+        document.seek(0)
+        return send_file(
+            document, mimetype='application/vnd.openxmlformats-'
+            'officedocument.wordprocessingml.document', as_attachment=True,
+            attachment_filename='Residential_'+str(datetime.datetime.now())+'.docx')
+    else:
+        return render_template("residential.html")
 
 @app.route("/getCookie", methods=['GET'])
 def getCookie():
@@ -102,4 +165,3 @@ def getCookie():
  
 if __name__ == "__main__":
     app.run(debug=True)
-
